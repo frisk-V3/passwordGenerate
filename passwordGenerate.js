f2c3f38b7e05d23ab7dab3440d527e587163a39e
frisk-V3 <asahiharuto@outlook.jp>
2026-02-13 15:19:07 +0900

    Add files via upload

---
+++ passwordGenerate.js
@@ -0,0 +1,14 @@
+﻿const generatePassword = (length = 16) => {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  // 安全な乱数を生成
+  const randomValues = new Uint32Array(length);
+  window.crypto.getRandomValues(randomValues);
+
+
+  return Array.from(randomValues)
+    .map((x) => charset[x % charset.length])
+    .join('');
+};
+
+
+console.log(generatePassword(20)); // 20桁のパスワードを表示
